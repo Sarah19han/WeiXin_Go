f153cd8c52b91c2eff667af030d677f91265f5c8
Sarah19han <87214216+Sarah19han@users.noreply.github.com>
2023-08-31 09:22:54 +0800

    修改源代码

--- Dockerfile
+++ Dockerfile
@@ -27,6 +27,26 @@ WORKDIR /app
 COPY --from=builder /app/main /app/index.html /app/
 
 # 执行启动命令
+CMD ["/app/main"]
 # 写多行独立的CMD命令是错误写法！只有最后一行CMD命令会被执行，之前的都会被忽略，导致业务报错。
 # 请参考[Docker官方文档之CMD命令](https://docs.docker.com/engine/reference/builder/#cmd)
-CMD ["/app/main"]
+
+# ---------------------------
+# CMD ["/app/main"]
+# FROM golang:1.13 as builder
+
+# RUN mkdir /app
+
+# ADD . /app/
+
+# WORKDIR /app
+
+# RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o main .
+
+# FROM alpine:latest
+
+# WORKDIR /app
+
+# COPY --from=builder /app/main .
+
+# CMD ["/app/main"]
--- main.go
+++ main.go
@@ -4,17 +4,24 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"wxcloudrun-golang/db"
-	"wxcloudrun-golang/service"
+	"os"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	log.Print("收到访问请求！")
+	target := "欢迎使用微信云托管"
+	fmt.Fprintf(w, "Hello, %s!\n", target)
+}
+
 func main() {
-	if err := db.Init(); err != nil {
-		panic(fmt.Sprintf("mysql init failed with %+v", err))
+	log.Print("微信云托管服务启动成功")
+
+	http.HandleFunc("/", handler)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "80"
 	}
 
-	http.HandleFunc("/", service.IndexHandler)
-	http.HandleFunc("/api/count", service.CounterHandler)
-
-	log.Fatal(http.ListenAndServe(":80", nil))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
